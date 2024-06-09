a71068df872a92fd50bb7744d3da77d78507895e
Lucas Goudin <goudinlucas@gmail.com>
2024-06-09 13:58:01 +0200

    test: added missing unit tests for GameGrid and GameState classes

--- test/features/game/presentation/bloc/game/game_state_test.dart
+++ test/features/game/presentation/bloc/game/game_state_test.dart
@@ -3,6 +3,86 @@ import 'package:tic_tac_toe/features/game/domain/entities/entities.dart';
 import 'package:tic_tac_toe/features/game/presentation/bloc/game/game_cubit.dart';
 
 void main() {
+  group('GameState.initialize', () {
+    test('should generate a new GameState', () {
+      final GameState state = GameState.initialize(
+        size: 3,
+        playerNames: const <String>['Player 1', 'Player 2'],
+      );
+
+      expect(state.grid.size, 3);
+      expect(state.players.length, 2);
+      expect(state.players[0].name, 'Player 1');
+      expect(state.players[0].index, 0);
+      expect(state.players[1].name, 'Player 2');
+      expect(state.players[1].index, 1);
+      expect(state.currentPlayerId, 0);
+      expect(state.isGameOver, false);
+      expect(state.winner, isNull);
+    });
+  });
+
+  group('canIPlay', () {
+    test("should return true if it is the given player's turn", () {
+      final GameState state = GameState(
+        grid: GameGrid.generate(size: 3),
+        players: const <Player>[
+          Player(index: 0, name: 'Player 1'),
+          Player(index: 1, name: 'Player 2'),
+        ],
+      );
+
+      final bool canIPlay = state.canIPlay(0);
+
+      expect(canIPlay, true);
+    });
+
+    test("should return false if it is not the given player's turn", () {
+      final GameState state = GameState(
+        grid: GameGrid.generate(size: 3),
+        players: const <Player>[
+          Player(index: 0, name: 'Player 1'),
+          Player(index: 1, name: 'Player 2'),
+        ],
+        currentPlayerId: 1,
+      );
+
+      final bool canIPlay = state.canIPlay(0);
+
+      expect(canIPlay, false);
+    });
+  });
+
+  group('getPlayerCellValue', () {
+    test('should return the correct cell value (1)', () {
+      final GameState state = GameState(
+        grid: GameGrid.generate(size: 3),
+        players: const <Player>[
+          Player(index: 0, name: 'Player 1'),
+          Player(index: 1, name: 'Player 2'),
+        ],
+      );
+
+      final GameGridCellValue cellValue = state.getPlayerCellValue(0);
+
+      expect(cellValue, GameGridCellValue.cross);
+    });
+
+    test('should return the correct cell value (2)', () {
+      final GameState state = GameState(
+        grid: GameGrid.generate(size: 3),
+        players: const <Player>[
+          Player(index: 0, name: 'Player 1'),
+          Player(index: 1, name: 'Player 2'),
+        ],
+      );
+
+      final GameGridCellValue cellValue = state.getPlayerCellValue(1);
+
+      expect(cellValue, GameGridCellValue.circle);
+    });
+  });
+
   group('getWinner', () {
     test('should return null if there are no winners', () {
       final List<Player> players = <Player>[
