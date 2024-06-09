83c5ef568aec363e8182cd11e50b72af13329207
Lucas Goudin <38526173+Rukkaitto@users.noreply.github.com>
2024-06-09 15:21:00 +0200

    Ajout de niveaux de difficulté du joueur IA (#4)
    Co-authored-by: Lucas Goudin <goudinlucas@gmail.com>

--- lib/core/services/computer_player_service/computer_player_service.dart
+++ lib/core/services/computer_player_service/computer_player_service.dart
@@ -5,9 +5,13 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../features/game/domain/entities/entities.dart';
 import '../../../features/game/presentation/bloc/game/game_cubit.dart';
+import 'computer_player_difficulty.dart';
 
 class ComputerPlayerService {
+  ComputerPlayerService({required this.difficulty});
+
   final Random _random = Random();
+  final ComputerPlayerDifficulty difficulty;
 
   Future<void> makeMove(BuildContext context) async {
     final GameCubit gameCubit = context.read<GameCubit>();
@@ -15,12 +19,18 @@ class ComputerPlayerService {
     // Wait for a random duration to simulate thinking (between 1 and 2 seconds)
     await Future<void>.delayed(Duration(seconds: _random.nextInt(2) + 1));
 
-    final int bestMove = _findBestMove(gameCubit.state);
+    int move;
 
-    gameCubit.play(playerId: 1, index: bestMove);
+    if (difficulty == ComputerPlayerDifficulty.easy) {
+      move = _findRandomMove(gameCubit.state);
+    } else {
+      move = _findBestMove(gameCubit.state);
+    }
+
+    gameCubit.play(playerId: 1, index: move);
   }
 
-  int _getScore(GameState state, int depth) {
+  int _evaluate(GameState state, int depth) {
     if (state.isGameOver) {
       if (state.winner == null) {
         return 0;
@@ -35,8 +45,20 @@ class ComputerPlayerService {
   }
 
   int _minimax(GameState state, int depth, bool isMax) {
-    if (state.isGameOver) {
-      return _getScore(state, depth);
+    // Limits the depth of the search depending on the difficulty
+    switch (difficulty) {
+      case ComputerPlayerDifficulty.easy:
+        if (depth == 0) {
+          return _evaluate(state, depth);
+        }
+      case ComputerPlayerDifficulty.medium:
+        if (depth == 1) {
+          return _evaluate(state, depth);
+        }
+      case ComputerPlayerDifficulty.hard:
+        if (depth == 2) {
+          return _evaluate(state, depth);
+        }
     }
 
     if (isMax) {
@@ -92,6 +114,18 @@ class ComputerPlayerService {
 
       return bestScore;
     }
+  }
+
+  int _findRandomMove(GameState state) {
+    final List<int> emptyCells = <int>[];
+
+    for (int i = 0; i < state.grid.length; i++) {
+      if (state.grid.getCell(i).value == GameGridCellValue.empty) {
+        emptyCells.add(i);
+      }
+    }
+
+    return emptyCells[_random.nextInt(emptyCells.length)];
   }
 
   int _findBestMove(GameState state) {
